b99cd02a8b54da90f3a9ab974b7e1daaca264473
Corentin Vannier <corentin.vannier@cea.fr>
2023-06-23 16:52:58 +0200

    Fix empty library view

--- ui/apps/annotator/src/lib/EmptyLibrary.svelte
+++ ui/apps/annotator/src/lib/EmptyLibrary.svelte
@@ -15,10 +15,24 @@
   */
 </script>
 
+<div class="mt-4 flex w-full justify-center font-bold italic text-zinc-500">
+  Loading datasets...
+</div>
 <div
-  class="w-full h-full pt-10 flex flex-col justify-center items-center text-lg text-zinc-500"
+  class="mt-8 mx-auto border border-zinc-100 shadow rounded-md p-4 max-w-sm w-full
+  dark:border-zinc-700"
 >
-  <img src="icons/sad.svg" alt="sad icon" class="h-16 w-16 mb-4 opacity-50" />
-  <span> We could not find any datasets. </span>
-  <span> Have you specified the right path ? </span>
+  <div class="animate-pulse flex space-x-4">
+    <div class="rounded-full bg-slate-200 h-10 w-10" />
+    <div class="flex-1 space-y-6 py-1">
+      <div class="h-2 bg-slate-200 rounded" />
+      <div class="space-y-3">
+        <div class="grid grid-cols-3 gap-4">
+          <div class="h-2 bg-slate-200 rounded col-span-2" />
+          <div class="h-2 bg-slate-200 rounded col-span-1" />
+        </div>
+        <div class="h-2 bg-slate-200 rounded" />
+      </div>
+    </div>
+  </div>
 </div>
