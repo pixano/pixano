915ac3bad1f632bd22282cecd455656715dc9e00
Corentin Vannier <corentin.vannier@cea.fr>
2023-07-04 11:20:06 +0200

    Remove unused code

--- ui/apps/annotator/vite.config.ts
+++ ui/apps/annotator/vite.config.ts
@@ -2,7 +2,6 @@
 
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
