074eb4a012b4a4a112176b79ab4bd496c8cc84a4
Corentin Vannier <corentin.vannier@cea.fr>
2023-07-05 15:13:09 +0200

    Move display function inside PixanoApp

--- pixano/apps/core/serve.py
+++ pixano/apps/core/serve.py
@@ -12,20 +12,22 @@
 # http://www.cecill.info
 
 import asyncio
+import html
+import json
 from pathlib import Path
 
 import fastapi
+import IPython.display
+import shortuuid
 import uvicorn
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from pixano import notebook
-
 from .main import Settings, create_app
 
 
-def _get_env() -> str:
+def get_env() -> str:
     """Get current running environment
 
     Returns:
@@ -56,13 +58,109 @@ def _get_env() -> str:
     return "none"
 
 
+def display_colab(url: str, port: int, height: int):
+    """Display a Pixano app inside a Google Colab
+
+    Args:
+        url (str): Pixano app URL
+        port (int): Pixano app port
+        height (int): Frame height
+    """
+
+    # Define frame template
+    shell = """
+        (() => {
+            const url = new URL(%URL%);
+            const port = %PORT%;
+            if (port) {
+                url.port = port;
+            }
+            const iframe = document.createElement('iframe');
+            iframe.src = url;
+            iframe.setAttribute('width', '100%');
+            iframe.setAttribute('height', '%HEIGHT%');
+            iframe.setAttribute('frameborder', 0);
+            document.body.appendChild(iframe);
+        })();
+    """
+
+    # Replace variables in template
+    replacements = [
+        ("%HEIGHT%", "%d" % height),
+        ("%PORT%", "%d" % port),
+        ("%URL%", json.dumps(url)),
+    ]
+    for k, v in replacements:
+        shell = shell.replace(k, v)
+
+    # Display frame
+    script = IPython.display.Javascript(shell)
+    IPython.display.display(script)
+
+
+def display_ipython(url: str, port: int, height: int):
+    """Display a Pixano app inside a Jupyter notebook
+
+    Args:
+        url (str): Pixano app URL
+        port (int): Pixano app port
+        height (int): Frame height
+    """
+
+    # Define frame template
+    shell = """
+        <iframe id="%HTML_ID%" width="100%" height="%HEIGHT%" frameborder="0">
+        </iframe>
+        <script>
+            (function() {
+                const frame = document.getElementById(%JSON_ID%);
+                const url = new URL(%URL%, window.location);
+                const port = %PORT%;
+                if (port) {
+                    url.port = port;
+                }
+                frame.src = url;
+            })();
+        </script>
+    """
+
+    # Replace variables in template
+    frame_id = f"frame-{shortuuid.uuid()}"
+    replacements = [
+        ("%HTML_ID%", html.escape(frame_id, quote=True)),
+        ("%JSON_ID%", json.dumps(frame_id)),
+        ("%HEIGHT%", "%d" % height),
+        ("%PORT%", "%d" % port),
+        ("%URL%", json.dumps(url)),
+    ]
+    for k, v in replacements:
+        shell = shell.replace(k, v)
+
+    # Display frame
+    iframe = IPython.display.HTML(shell)
+    IPython.display.display(iframe)
+
+
+def display_cli(url: str, port: int, height: int):
+    """Display a Pixano app inside a command line interface
+
+    Args:
+        url (str): Pixano app URL
+        port (int): Pixano app port
+        height (int): Frame height
+    """
+
+    print(f"Please visit {url}:{port} in a web browser.")
+
+
 class PixanoApp:
     """Pixano App
 
     Attributes:
         config (uvicorn.Config): App config
         server (uvicorn.Server): App server
-        env (function): Run task function for running environment
+        task_function (function): Run task function for running environment
+        display_function (function): Display function for running environment
     """
 
     def __init__(
@@ -81,6 +179,7 @@ class PixanoApp:
             port (int, optional): App port. Defaults to 8000.
         """
 
+        # Create app
         templates = Jinja2Templates(directory=template_path)
         settings = Settings(data_dir=Path(library_dir))
         app = create_app(settings)
@@ -93,21 +192,36 @@ class PixanoApp:
         self.config = uvicorn.Config(app, host=host, port=port)
         self.server = uvicorn.Server(self.config)
 
-        self.env = {
+        # Get environmennt
+        self.task_function = {
             "colab": asyncio.get_event_loop().create_task,
             "ipython": asyncio.get_event_loop().create_task,
             "none": asyncio.run,
-        }[_get_env()]
+        }[get_env()]
+        self.display_function = {
+            "colab": display_colab,
+            "ipython": display_ipython,
+            "none": display_cli,
+        }[get_env()]
 
-        self.env(self.server.serve())
+        # Serve app
+        self.task_function(self.server.serve())
 
-    def display(self) -> None:
-        """Display Pixano app"""
+    def display(self, height: int = 1000) -> None:
+        """Display Pixano app
 
+        Args:
+            height (int, optional): Frame height. Defaults to 1000.
+        """
+
+        # Wait for app to be online
         while not self.server.started:
-            self.env(asyncio.wait(0.1))
+            self.task_function(asyncio.wait(0.1))
 
+        # Display app
         for server in self.server.servers:
             for socket in server.sockets:
                 address = socket.getsockname()
-                notebook.display(url=f"http://{address[0]}", port=address[1])
+                self.display_function(
+                    url=f"http://{address[0]}", port=address[1], height=height
+                )
