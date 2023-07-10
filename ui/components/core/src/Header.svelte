96f832c0af319654f9be5a095715553bd99bb5d9
Corentin Vannier <corentin.vannier@cea.fr>
2023-07-10 17:05:33 +0200

    Fix svg icons in Header

--- ui/components/core/src/Header.svelte
+++ ui/components/core/src/Header.svelte
@@ -16,9 +16,6 @@
 
   // Assets
   import pixanoLogo from "./assets/pixano.png";
-  import svg_next from "./assets/icons/next.svg";
-  import svg_close from "./assets/icons/close.svg";
-  import svg_save from "./assets/icons/save.svg";
 
   // Imports
   import { createEventDispatcher } from "svelte";
@@ -52,26 +49,37 @@
 <!-- Header -->
 <header class="w-full fixed">
   <div
-    class="h-20 py-4 px-4 flex justify-start items-center shrink-0 border-b-2
+    class="h-20 z-50 py-4 px-4 flex justify-start items-center shrink-0 border-b
     text-zinc-800 dark:text-zinc-300
     bg-white dark:bg-zinc-800
-    border-zinc-200 dark:border-zinc-600"
+    border-zinc-300 dark:border-zinc-500"
   >
     <!-- Logo & app name -->
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div class="flex items-center grow space-x-2 font-bold text-3xl">
       <button
         class="cursor-pointer flex items-center space-x-2
-        hover:text-rose-500 dark:hover:text-rose-600"
+        hover:text-rose-600 dark:hover:text-rose-500"
         on:click={unselectDataset}
       >
         <img src={pixanoLogo} alt="Logo Pixano" class="w-10" />
         <span class="transition-colors"> Pixano {app} </span>
       </button>
       {#if selectedDataset}
-        <img src={svg_next} alt="icon" class="h-6 w-6 opacity-75" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="48"
+          viewBox="0 -960 960 960"
+          width="48"
+          class="h-6 w-6 text-zinc-800 dark:text-zinc-300"
+        >
+          <path
+            d="m375-240-43-43 198-198-198-198 43-43 241 241-241 241Z"
+            fill="currentcolor"
+          />
+        </svg>
         <button
-          class="hover:text-rose-500 dark:hover:text-rose-600"
+          class="hover:text-rose-600 dark:hover:text-rose-500"
           on:click={handleCloseClick}
         >
           <span class="transition-colors">
@@ -79,7 +87,18 @@
           </span>
         </button>
         {#if selectedItem}
-          <img src={svg_next} alt="icon" class="h-6 w-6 opacity-75" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="48"
+            viewBox="0 -960 960 960"
+            width="48"
+            class="h-6 w-6 text-zinc-800 dark:text-zinc-300"
+          >
+            <path
+              d="m375-240-43-43 198-198-198-198 43-43 241 241-241 241Z"
+              fill="currentcolor"
+            />
+          </svg>
           <span>
             {selectedItem.id}
           </span>
@@ -91,25 +110,40 @@
     {#if selectedDataset}
       {#if selectedItem && app === "Annotator"}
         <button class="w-30 h pr-4 flex justify-end" on:click={handleSaveClick}>
-          <img
-            src={svg_save}
-            alt="icon"
-            class="h-8 w-8 cursor-pointer cursor-pointer opacity-75"
-            style={save_flag
-              ? "background-color: #ffe4e6; box-shadow: 0px 0px 5px 5px #ffe4e6;"
-              : ""}
-          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="48"
+            viewBox="0 -960 960 960"
+            width="48"
+            class="hover:text-rose-600 dark:hover:text-rose-500 h-8 w-8
+            {save_flag ? 'text-rose-500 dark:text-rose-600' : ''}"
+          >
+            <title>Save</title>
+            <path
+              d="M840-683v503q0 24-18 42t-42 18H180q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h503l157 157Zm-60 27L656-780H180v600h600v-476ZM479.765-245Q523-245 553.5-275.265q30.5-30.264 30.5-73.5Q584-392 553.735-422.5q-30.264-30.5-73.5-30.5Q437-453 406.5-422.735q-30.5 30.264-30.5 73.5Q376-306 406.265-275.5q30.264 30.5 73.5 30.5ZM233-584h358v-143H233v143Zm-53-72v476-600 124Z"
+              fill="currentcolor"
+            />
+          </svg>
         </button>
       {/if}
       <button
         class="w-30 pr-4 flex justify-end"
         on:click={selectedItem ? handleCloseClick : unselectDataset}
       >
-        <img
-          src={svg_close}
-          alt="icon"
-          class="h-8 w-8 cursor-pointer opacity-75"
-        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="48"
+          viewBox="0 -960 960 960"
+          width="48"
+          class="h-8 w-8 hover:text-rose-600 dark:hover:text-rose-500"
+          name="close"
+        >
+          <title>Close</title>
+          <path
+            d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"
+            fill="currentcolor"
+          />
+        </svg>
       </button>
     {/if}
   </div>
