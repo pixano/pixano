893bab10254be7e67cd69e30c62e41a7977a97ba
Corentin Vannier <corentin.vannier@cea.fr>
2023-07-27 16:50:29 +0200

    Update label color function

--- ui/components/core/src/utils.ts
+++ ui/components/core/src/utils.ts
@@ -18,14 +18,6 @@ import { scaleOrdinal } from "d3";
 import { schemeSet3 } from "d3-scale-chromatic";
 
 // Exports
-/**
- * Generates an unique color based on a given id.
- * @param id the id
- */
-export function getColor(classes) {
-  let classes_range = [
-    Math.min(...classes.map((cat) => cat.id)),
-    Math.max(...classes.map((cat) => cat.id)),
-  ];
-  return scaleOrdinal(schemeSet3).domain(classes_range);
+export function colorLabel(range: Array<number>) {
+  return scaleOrdinal(schemeSet3).domain(range);
 }
