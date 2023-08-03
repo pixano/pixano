6a4bfc877198fa352b2dd83ad723fbd0d1003dd0
Corentin Vannier <corentin.vannier@cea.fr>
2023-08-03 09:49:53 +0200

    Hande import errors for boptoolkit and webdataset

--- pixano/data/importers/__init__.py
+++ pixano/data/importers/__init__.py
@@ -1,6 +1,18 @@
-from .coco_importer import COCO_Importer
+# @Copyright: CEA-LIST/DIASI/SIALV/LVA (2023)
+# @Author: CEA-LIST/DIASI/SIALV/LVA <pixano@cea.fr>
+# @License: CECILL-C
+#
+# This software is a collaborative computer program whose purpose is to
+# generate and explore labeled data for computer vision applications.
+# This software is governed by the CeCILL-C license under French law and
+# abiding by the rules of distribution of free software. You can use,
+# modify and/ or redistribute the software under the terms of the CeCILL-C
+# license as circulated by CEA, CNRS and INRIA at the following URL
+#
+# http://www.cecill.info
 
-# from .bopWDS_importer import BopWDS_Importer
+from .bopWDS_importer import BopWDS_Importer
+from .coco_importer import COCO_Importer
 from .dota_importer_test import DOTA_Importer
 from .image_importer import Image_Importer
 from .importer import Importer
@@ -8,7 +20,7 @@ from .importer import Importer
 __all__ = [
     "Importer",
     "Image_Importer",
-    # "BopWDS_Importer",
+    "BopWDS_Importer",
     "DOTA_Importer",
     "COCO_Importer",
 ]
