64a9c355a245b55cb5e196df560faf9925bb5b0d
Corentin Vannier <corentin.vannier@cea.fr>
2023-10-05 09:07:18 +0200

    Update classification icon

--- ui/components/core/src/icons.ts
+++ ui/components/core/src/icons.ts
@@ -44,5 +44,5 @@ export const svg_point_plus =
   "M450-200v-250H200v-60h250v-250h60v250h250v60H510v250h-60Z";
 export const svg_point_minus = "M200-450v-60h560v60H200Z";
 
-export const svg_classify = "M200-450v-60h560v60H200Z";  //TODO change icon
-
+export const svg_classify =
+  "M840-480 666-234q-11 16-28.5 25t-37.5 9H200q-33 0-56.5-23.5T120-280v-400q0-33 23.5-56.5T200-760h400q20 0 37.5 9t28.5 25l174 246Zm-98 0L600-680H200v400h400l142-200Zm-542 0v200-400 200Z";
