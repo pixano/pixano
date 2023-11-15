5b03c22b005d8c9472f19c0f7b37eaf939062cd9
Timothee Reynaud <timothee.r@lajavaness.com>
2023-11-15 15:58:00 +0100

    chore(front): Add code linting and formatting, #2

---
+++ ui/.eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:svelte/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
+  env: {
+    es2022: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./**/tsconfig.json"],
+    extraFileExtensions: [".svelte"],
+  },
+  ignorePatterns: [
+    ".eslintrc.cjs",
+    ".prettierrc.cjs",
+    "svelte.config.js",
+    "tailwind.config.cjs",
+    "postcss.config.cjs",
+    "vite.config.ts",
+  ],
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  ],
+  rules: {
+    "no-undef": "off",
+    "prefer-const": "error",
+    "no-unused-vars": "error",
+  },
+};
---
+++ ui/.prettierrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  useTabs: true,
+  doubleQuote: true,
+  trailingComma: "all",
+  printWidth: 100,
+  plugins: ["prettier-plugin-svelte"],
+};
