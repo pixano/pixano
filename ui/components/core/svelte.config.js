213ee2adbcfaf73cd2a7bf9e22b3549926a8bf18
Timothee Reynaud <timothee.r@lajavaness.com>
2023-11-21 14:04:05 +0100

    chore(front): Adding shadcn to core

--- ui/components/core/svelte.config.js
+++ ui/components/core/svelte.config.js
@@ -4,4 +4,9 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  kit: {
+    alias: {
+      $lib: "./src/lib",
+    },
+  },
 };
