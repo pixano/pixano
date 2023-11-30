98c0695fae73d3186ada1fbc279c128872b4ecd3
Corentin Vannier <corentin.vannier@cea.fr>
2023-11-30 16:52:49 +0100

    feat(api): Replace old API by new API

--- pixano/apps/main.py
+++ pixano/apps/main.py
@@ -4,7 +4,6 @@
 #
 # This software is a collaborative computer program whose purpose is to
 # generate and explore labeled data for computer vision applications.
-#
 # This software is governed by the CeCILL-C license under French law and
 # abiding by the rules of distribution of free software. You can use,
 # modify and/ or redistribute the software under the terms of the CeCILL-C
@@ -12,25 +11,14 @@
 #
 # http://www.cecill.info
 
-from fastapi import Depends, FastAPI, HTTPException, Response
+
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from fastapi_pagination import Page, Params
 from fastapi_pagination.api import add_pagination
 
-from pixano.api import (
-    ItemFeatures,
-    Settings,
-    load_dataset,
-    load_dataset_list,
-    load_dataset_stats,
-    load_item_details,
-    load_item_embeddings,
-    load_items,
-    save_item_details,
-    search_query,
-)
-from pixano.data import DatasetInfo
+from pixano.apps.api import datasets, items, models
+from pixano.data import Settings
 
 
 def create_app(settings: Settings = Settings()) -> FastAPI:
@@ -38,10 +26,6 @@ def create_app(settings: Settings = Settings()) -> FastAPI:
 
     Args:
         settings (Settings, optional): Settings containing dataset library path. Defaults to empty Settings().
-
-    Raises:
-        FileNotFoundError: Dataset library not found
-        HTTPException: Dataset / dataset stats / dataset items not found
 
     Returns:
         FastAPI: Pixano App
@@ -73,99 +57,9 @@ def create_app(settings: Settings = Settings()) -> FastAPI:
         name="data",
     )
 
-    @app.get("/datasets", response_model=list[DatasetInfo])
-    async def get_dataset_list():
-        # Load dataset list
-        return load_dataset_list(settings)
-
-    @app.get("/datasets/{ds_id}", response_model=DatasetInfo)
-    async def get_dataset(ds_id: str):
-        # Load dataset
-        ds = load_dataset(ds_id, settings)
-        if ds is None:
-            raise HTTPException(status_code=404, detail="Dataset not found")
-        else:
-            return ds.info
-
-    @app.get("/datasets/{ds_id}/items", response_model=Page[ItemFeatures])
-    async def get_dataset_items(ds_id: str, params: Params = Depends()):
-        # Load dataset
-        ds = load_dataset(ds_id, settings)
-        if ds is None:
-            raise HTTPException(status_code=404, detail="Dataset not found")
-        else:
-            # Load dataset items
-            res = load_items(ds, params)
-            if res is None:
-                raise HTTPException(status_code=404, detail="Dataset items not found")
-            else:
-                return res
-
-    @app.get("/datasets/{ds_id}/stats")
-    async def get_dataset_stats(ds_id: str):
-        # Load dataset
-        ds = load_dataset(ds_id, settings)
-        if ds is None:
-            raise HTTPException(status_code=404, detail="Dataset not found")
-        else:
-            # Load dataset stats
-            stats = load_dataset_stats(ds, settings)
-            if stats is None:
-                raise HTTPException(status_code=404, detail="Dataset stats not found")
-            else:
-                return stats
-
-    @app.get("/datasets/{ds_id}/items/{item_id}")
-    async def get_item_details(ds_id: str, item_id: str):
-        # Load dataset
-        ds = load_dataset(ds_id, settings)
-        if ds is None:
-            raise HTTPException(status_code=404, detail="Dataset not found")
-        else:
-            # Load item objects
-            return load_item_details(ds, item_id)
-
-    @app.get("/datasets/{ds_id}/items/{item_id}/embeddings")
-    async def get_item_embeddings(ds_id: str, item_id: str):
-        # Load dataset
-        ds = load_dataset(ds_id, settings)
-        if ds is None:
-            raise HTTPException(status_code=404, detail="Dataset not found")
-        else:
-            # Load item embeddings
-            return load_item_embeddings(ds, item_id)
-
-    @app.post(
-        "/datasets/{ds_id}/items/{item_id}/details",
-        response_model=dict[str, list],
-    )
-    async def post_item_details(
-        ds_id: str,
-        item_id: str,
-        item_details: dict[str, list],
-    ):
-        # Load dataset
-        ds = load_dataset(ds_id, settings)
-        if ds is None:
-            raise HTTPException(status_code=404, detail="Dataset not found")
-        else:
-            # Save item objects
-            save_item_details(ds, item_id, item_details)
-            return Response()
-
-    @app.post(
-        "/datasets/{ds_id}/search",
-        response_model=Page[ItemFeatures],
-    )
-    async def post_search_query(
-        ds_id: str, query_rep: dict, params: Params = Depends()
-    ):
-        # Load dataset
-        ds = load_dataset(ds_id, settings)
-        if ds is None:
-            raise HTTPException(status_code=404, detail="Dataset not found")
-        else:
-            return search_query(ds, query_rep["query"], params)
+    app.include_router(datasets.router)
+    app.include_router(items.router)
+    app.include_router(models.router)
 
     add_pagination(app)
     return app
