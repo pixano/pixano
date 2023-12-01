c708d49330d2e0e21d62197e2e937275ec5f71ff
Corentin Vannier <corentin.vannier@cea.fr>
2023-12-01 10:40:08 +0100

    fix(tests): Update apps tests for new API

--- tests/apps/test_main.py
+++ tests/apps/test_main.py
@@ -13,25 +13,25 @@
 # http://www.cecill.info
 
 import json
+import shutil
 import tempfile
 import unittest
 from pathlib import Path
 
 from fastapi.testclient import TestClient
 
-from pixano.api import Settings
 from pixano.apps import create_app
-from pixano.data import COCOImporter
+from pixano.data import COCOImporter, DatasetInfo, DatasetItem, DatasetStat, Settings
 
 
 class AppTestCase(unittest.TestCase):
     def setUp(self):
         # Create temporary directory
-        self.temp_dir = tempfile.TemporaryDirectory()
-        library_dir = Path(self.temp_dir.name)
+        self.temp_dir = Path.cwd() / "library"
+        self.temp_dir.mkdir(exist_ok=False)
 
         # Create a COCO dataset
-        import_dir = library_dir / "coco"
+        import_dir = self.temp_dir / "coco"
         input_dirs = {
             "image": Path("tests/assets/coco_dataset/image"),
             "objects": Path("tests/assets/coco_dataset"),
@@ -50,51 +50,48 @@ class AppTestCase(unittest.TestCase):
 
         # Create dataset stats
         stats = [
-            {
-                "name": "Some numerical statistics",
-                "type": "numerical",
-                "histogram": [
+            DatasetStat(
+                name="Some numerical statistics",
+                type="numerical",
+                histogram=[
                     {"bin_start": 0.0, "bin_end": 1.0, "counts": 2, "split": "train"},
                     {"bin_start": 1.0, "bin_end": 2.0, "counts": 4, "split": "train"},
                     {"bin_start": 2.0, "bin_end": 3.0, "counts": 6, "split": "train"},
                     {"bin_start": 3.0, "bin_end": 4.0, "counts": 8, "split": "train"},
                 ],
-                "range": [0.0, 10.0],
-            },
-            {
-                "name": "Some categorical statistics",
-                "type": "categorical",
-                "histogram": [
+                range=[0.0, 10.0],
+            ),
+            DatasetStat(
+                name="Some categorical statistics",
+                type="categorical",
+                histogram=[
                     {"Some categorical statistics": "a", "counts": 2, "split": "train"},
                     {"Some categorical statistics": "b", "counts": 4, "split": "train"},
                     {"Some categorical statistics": "c", "counts": 6, "split": "train"},
                     {"Some categorical statistics": "d", "counts": 8, "split": "train"},
                 ],
-            },
+            ),
         ]
         with open(import_dir / "stats.json", "w", encoding="utf-8") as f:
-            json.dump(stats, f)
+            json.dump([stat.model_dump() for stat in stats], f)
 
         # Launch app
-        self.settings = Settings(data_dir=library_dir)
-        self.client = TestClient(create_app(self.settings))
+        self.client = TestClient(create_app(Settings()))
 
     def tearDown(self):
-        self.temp_dir.cleanup()
+        shutil.rmtree(self.temp_dir)
 
-    def test_get_dataset_list(self):
+    def test_get_datasets(self):
         response = self.client.get("/datasets")
         output = response.json()
 
         self.assertEqual(response.status_code, 200)
 
-        self.assertGreaterEqual(len(output), 1)
+        self.assertEqual(len(output), 1)
 
         for ds in output:
-            self.assertIn("id", ds)
-            self.assertIn("name", ds)
-            self.assertIn("description", ds)
-            self.assertIn("preview", ds)
+            ds_info = DatasetInfo.model_validate(ds)
+            self.assertIsInstance(ds_info, DatasetInfo)
 
     def test_get_dataset(self):
         response = self.client.get("/datasets/coco_dataset")
@@ -102,10 +99,8 @@ class AppTestCase(unittest.TestCase):
 
         self.assertEqual(response.status_code, 200)
 
-        self.assertIn("id", output)
-        self.assertIn("name", output)
-        self.assertIn("description", output)
-        self.assertIn("preview", output)
+        ds_info = DatasetInfo.model_validate(output)
+        self.assertIsInstance(ds_info, DatasetInfo)
 
     def test_get_dataset_items(self):
         response = self.client.get("/datasets/coco_dataset/items")
@@ -113,54 +108,110 @@ class AppTestCase(unittest.TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+        self.assertIn("items", output)
         self.assertIn("total", output)
         self.assertIn("page", output)
         self.assertIn("size", output)
         self.assertIn("pages", output)
 
-    def test_get_dataset_stats(self):
-        response = self.client.get("/datasets/coco_dataset/stats")
-        output = response.json()
+        self.assertEqual(len(output["items"]), 3)
 
-        self.assertEqual(response.status_code, 200)
+        for item in output["items"]:
+            ds_item = DatasetItem.model_validate(item)
+            self.assertIsInstance(ds_item, DatasetItem)
 
-        self.assertGreaterEqual(len(output), 1)
+    def test_search_dataset_items(self):
+        response = self.client.post(
+            "/datasets/coco_dataset/search",
+            headers={
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            json={"query": "bear"},
+        )
 
-        for stat in output:
-            self.assertIn("name", stat)
-            self.assertIn("type", stat)
-            self.assertIn("histogram", stat)
+        self.assertEqual(response.status_code, 404)
 
-    def test_get_item_details(self):
+        try:
+            from pixano_inference import transformers
+
+            model = transformers.CLIP()
+            model.process_dataset(
+                dataset_dir=self.temp_dir / "coco",
+                process_type="search_emb",
+                views=["image"],
+            )
+
+            response = self.client.post(
+                "/datasets/coco_dataset/search",
+                headers={
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                },
+                json={"query": "bear"},
+            )
+            output = response.json()
+
+            self.assertEqual(response.status_code, 200)
+
+            self.assertIn("items", output)
+            self.assertIn("total", output)
+            self.assertIn("page", output)
+            self.assertIn("size", output)
+            self.assertIn("pages", output)
+
+            self.assertEqual(len(output["items"]), 3)
+
+            for item in output["items"]:
+                ds_item = DatasetItem.model_validate(item)
+                self.assertIsInstance(ds_item, DatasetItem)
+
+        except ImportError:
+            print(
+                "Can't test search_items() fully without pixano-inference for CLIP embeddings"
+            )
+
+    def test_get_dataset_item(self):
         response = self.client.get("/datasets/coco_dataset/items/139")
         output = response.json()
 
         self.assertEqual(response.status_code, 200)
 
-        self.assertIn("itemData", output)
-        self.assertIn("itemObjects", output)
+        ds_item = DatasetItem.model_validate(output)
+        self.assertIsInstance(ds_item, DatasetItem)
 
-        self.assertIn("id", output["itemData"])
-        self.assertIn("views", output["itemData"])
-        self.assertIn("features", output["itemData"])
-        self.assertEqual(20, len(output["itemObjects"]))
+    def test_post_dataset_item(self):
+        response_1 = self.client.get("/datasets/coco_dataset/items/139")
+        output = response_1.json()
 
-    def test_post_item_details(self):
-        response = self.client.post(
-            "/datasets/coco_dataset/items/127/details",
+        ds_item = DatasetItem.model_validate(output)
+
+        response_2 = self.client.post(
+            "/datasets/coco_dataset/items/127",
             headers={
                 "Accept": "application/json",
                 "Content-Type": "application/json",
             },
-            json={
-                "itemData": [
-                    {"name": "feature", "dtype": "text", "value": "a text feature"}
-                ],
-                "itemObjects": [
-                    {"id": "object1", "source_id": "unit test"},
-                    {"id": "object2", "source_id": "unit test"},
-                ],
-            },
+            json=ds_item.model_dump(),
         )
 
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_2.status_code, 200)
+
+    def test_get_dataset_item_embeddings(self):
+        response = self.client.get("/datasets/coco_dataset/items/139/embeddings/SAM")
+
+        # TODO: Can't test embeddings without model weights
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_models(self):
+        with tempfile.NamedTemporaryFile(dir=self.temp_dir / "models", suffix=".onnx"):
+            response = self.client.get("/models")
+            output = response.json()
+
+            self.assertEqual(response.status_code, 200)
+
+            self.assertEqual(len(output), 1)
+
+            for model in output:
+                self.assertIsInstance(model, str)
+                self.assertIn(".onnx", model)
