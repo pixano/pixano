6b8969209ab698159166eea8387fd2f029301882
Timothee Reynaud <timothee.r@lajavaness.com>
2024-01-05 10:25:48 +0100

    style: Format pixano app code

--- ui/apps/pixano/vite.config.ts
+++ ui/apps/pixano/vite.config.ts
@@ -1,25 +1,25 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		proxy: {
-		"/datasets": {
-			target: "http://127.0.0.1:8000",
-			changeOrigin: true,
-			secure: false,
-		},
-		"/models": {
-			target: "http://127.0.0.1:8000",
-			changeOrigin: true,
-			secure: false,
-		},
-		"/data": {
-			target: "http://127.0.0.1:8000",
-			changeOrigin: true,
-			secure: false,
-		},
-		},
+  plugins: [sveltekit()],
+  server: {
+    proxy: {
+      "/datasets": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/models": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/data": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
