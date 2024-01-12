8f2eee93b453372464634f9cc205c45ce13a8104
Corentin Vannier <corentin.vannier@cea.fr>
2024-01-12 15:36:10 +0100

    chore(back): Remove unused import

--- pixano/app/main.py
+++ pixano/app/main.py
@@ -10,8 +10,6 @@
 #
 # http://www.cecill.info
 
-from pathlib import Path
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
