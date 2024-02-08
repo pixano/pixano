f4cf099055346b582ce84f8b54259b48ab961e42
Corentin Vannier <56968251+cpvannier@users.noreply.github.com>
2024-02-08 16:56:47 +0100

    fix(front): Fix buttons in DatasetHeader (#120)
    * fix(front): Add tooltips for navigation buttons
    
    * fix(front): Use back button to go to parent page
    
    - Go back to home page if on dataset page
    - Go back to dataset page if on item page
    - Pixano logo already goes back to home page regardless of current page

--- ui/apps/pixano/src/components/layout/DatasetHeader.svelte
+++ ui/apps/pixano/src/components/layout/DatasetHeader.svelte
@@ -105,10 +105,13 @@
         <button on:click={() => navigateTo("/")} class="h-10 w-10">
           <img src={pixanoLogo} alt="Logo Pixano" class="w-8 h-8 mx-2" />
         </button>
-        <IconButton on:click={() => navigateTo("/")}>
+        <IconButton
+          on:click={() => navigateTo(currentItemId ? `/${datasetName}/dataset` : "/")}
+          tooltipContent={currentItemId ? "Back to dataset" : "Back to home"}
+        >
           <ArrowLeftCircleIcon />
         </IconButton>
-        <button on:click={() => navigateTo(`/${datasetName}/dataset`)}> {datasetName} </button>
+        {datasetName}
       </div>
     </div>
     {#if currentItemId}
@@ -116,11 +119,11 @@
         <Loader2Icon class="animate-spin" />
       {:else}
         <div class="flex items-center gap-4">
-          <IconButton on:click={() => goToNeighborItem("previous")}>
+          <IconButton on:click={() => goToNeighborItem("previous")} tooltipContent="Previous item">
             <ArrowLeft />
           </IconButton>
           {currentItemId}
-          <IconButton on:click={() => goToNeighborItem("next")}>
+          <IconButton on:click={() => goToNeighborItem("next")} tooltipContent="Next item">
             <ArrowRight />
           </IconButton>
         </div>
