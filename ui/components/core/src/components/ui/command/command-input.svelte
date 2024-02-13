44c025f64959cad4543d0a87ef253aad3283c438
Timothee Reynaud <timothee.r@lajavaness.com>
2024-02-13 16:25:00 +0100

    feat: Save before refacto

--- ui/components/core/src/components/ui/command/command-input.svelte
+++ ui/components/core/src/components/ui/command/command-input.svelte
@@ -3,30 +3,24 @@
   import { Search } from "lucide-svelte";
   import { cn } from "../../../lib/utils/styleUtils";
 
-  type $$Props = CommandPrimitive.InputProps & {
-    value: string;
-    onValidation: (value: string) => void | null;
-  };
+  type $$Props = CommandPrimitive.InputProps;
 
   let className: string | undefined | null = undefined;
+  export let value: string = "";
   export { className as class };
-  export let value: string;
-  export let onValidation: (value: string) => void | null;
-
-  const onBlur = () => {
-    onValidation(value);
-  };
 </script>
 
 <div class="flex items-center border-b px-2" data-cmdk-input-wrapper="">
   <Search class="mr-2 h-4 w-4 shrink-0 opacity-50" />
   <CommandPrimitive.Input
+    on:input
     on:focus
-    on:blur={onBlur}
+    on:blur
     class={cn(
       "flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
       className,
     )}
     {...$$restProps}
+    bind:value
   />
 </div>
--- ui/components/imageWorkspace/src/components/Features/AutoCompleteFeature.svelte
+++ ui/components/imageWorkspace/src/components/Features/AutoCompleteFeature.svelte
@@ -1,61 +1,82 @@
 <script lang="ts">
   import { Check } from "lucide-svelte";
   import { Command, cn } from "@pixano/core/src";
-  import { tick } from "svelte";
 
   export let listItems: { value: string; label: string }[] = [];
   export let placeholder: string = "Select an item";
-
-  let open = false;
-  let inputValue: string = "";
   export let value: string = "";
+  // export let selectedValue: string = "";
   export let saveValue: (value: string) => void = () => {};
 
-  $: selectedValue = listItems.find((f) => f.value === value)?.label ?? placeholder;
+  let open = false;
+  // let inputStatus: "none" | "blur" | "focus" = "none";
+  let shouldClose: "no" | "hasBlurred" | "hasSelected" = "no";
 
-  $: console.log({ value, inputValue, selectedValue, listItems });
+  //   let inputValue: string = "";
 
-  $: {
-    if (value) saveValue(value);
-  }
+  //   $: selectedValue = listItems.find((f) => f.value === value)?.label ?? placeholder;
+
+  //   $: console.log({ value, inputValue, selectedValue, listItems });
+
+  //   $: {
+  //     if (value) saveValue(value);
+  //   }
 
   // We want to refocus the trigger button when the user selects
   // an item from the list so users can continue navigating the
   // rest of the form with the keyboard.
-  function closeAndFocusTrigger(triggerId: string) {
-    open = false;
-    tick()
-      .then(() => {
-        document.getElementById(triggerId)?.focus();
-      })
-      .catch((err) => console.error(err));
-  }
+  // async function closeAndFocusTrigger(triggerId: string) {
+  //   tick()
+  //     .then(() => {
+  //       open = false;
+  //       document.getElementById(triggerId)?.focus();
+  //     })
+  //     .catch((err) => console.error(err));
+  // }
 
-  const onValidation = (value: string) => {
-    selectedValue = value;
+  //   const onValidation = (value: string) => {
+  //     console.log("validation", { value });
+  //     selectedValue = value;
+  //     // open = false;
+  //   };
+
+  // $: {
+  //   if (shouldClose === "hasBlurred") {
+  //     open = false;
+  //     shouldClose = "no";
+  //   }
+  // }
+
+  const onSelect = (currentValue: string) => {
+    console.log("onSelect", { currentValue });
+    // selectedValue = currentValue;
+    value = currentValue;
     open = false;
   };
 
-  const onSelect = (currentValue: string) => {
-    value = currentValue;
-    closeAndFocusTrigger("ids.trigger");
+  // const onInput = () => {
+  //   selectedValue = "";
+  // };
+
+  const onInputBlur = () => {
+    if (!listItems.some((listItem) => listItem.value === value) && value !== "") {
+      console.log("blur", { value });
+      saveValue(value);
+    }
+    setTimeout(() => {
+      open = false;
+    }, 200);
   };
 </script>
 
-<Command.Root class="overflow-visible">
+<Command.Root class="overflow-visible" on:blur={() => console.log("blurred")}>
   <Command.Input
-    {onValidation}
-    bind:value={inputValue}
-    placeholder="Search framework..."
+    {placeholder}
+    bind:value
+    on:blur={onInputBlur}
+    on:focus={() => (open = true)}
     class="h-9"
-    on:focus={() => {
-      console.log("focus");
-      open = true;
-    }}
   />
-
-  <!-- <input class="h-9" placeholder="Search" bind:value={inputValue} /> -->
-  <!--<Command.Empty>No framework found.</Command.Empty>-->
   {#if open}
     <Command.Group class="z-10 bg-white absolute top-full w-full">
       {#each listItems as val}
@@ -67,38 +88,3 @@
     </Command.Group>
   {/if}
 </Command.Root>
-
-<!--
-  <Popover.Root bind:open let:ids>
-    <Popover.Trigger asChild let:builder>
-      <Button
-        builders={[builder]}
-        variant="outline"
-        role="combobox"
-        aria-expanded={open}
-        class={cn("justify-between", width)}
-      >
-        {selectedValue}
-        <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
-      </Button>
-    </Popover.Trigger>
-    <Popover.Content class={cn("p-0", width)}>
-      <Command.Root>
-        <Command.Group>
-          {#each listItems as listItem}
-            <Command.Item
-              value={listItem.value}
-              onSelect={(currentValue) => {
-                value = currentValue;
-                closeAndFocusTrigger(ids.trigger);
-              }}
-            >
-              <Check class={cn("mr-2 h-4 w-4", value !== listItem.value && "text-transparent")} />
-              {listItem.label}
-            </Command.Item>
-          {/each}
-        </Command.Group>
-      </Command.Root>
-    </Popover.Content>
-  </Popover.Root>
-  -->
