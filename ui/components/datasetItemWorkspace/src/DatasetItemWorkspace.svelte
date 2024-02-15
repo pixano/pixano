2f4d3eafc67395c2086d9cac6e1fa1646bc7ed75
Timothee Reynaud <timothee.r@lajavaness.com>
2024-02-15 16:58:23 +0100

    refactor(front): Rename ImageInspector to Inspector

--- ui/components/datasetItemWorkspace/src/DatasetItemWorkspace.svelte
+++ ui/components/datasetItemWorkspace/src/DatasetItemWorkspace.svelte
@@ -25,7 +25,7 @@
 
   import Toolbar from "./components/Toolbar.svelte";
   import ImageCanvas from "./components/ImageCanvas.svelte";
-  import ImageInspector from "./components/ImageInspector/ImageInspector.svelte";
+  import Inspector from "./components/Inspector/InspectorInspector.svelte";
   import LoadModelModal from "./components/LoadModelModal.svelte";
   import {
     itemObjects,
@@ -131,7 +131,7 @@
     {embeddings}
     {isLoading}
   />
-  <ImageInspector on:click={onSave} {isLoading} />
+  <Inspector on:click={onSave} {isLoading} />
   <LoadModelModal
     {models}
     currentDatasetId={currentDataset.id}
