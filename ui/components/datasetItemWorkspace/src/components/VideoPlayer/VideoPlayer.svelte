aaff99dc91f6da89c6174ad063a1c4e671e09716
Timothee Reynaud <timothee.r@lajavaness.com>
2024-03-01 12:36:19 +0100

    feat(front): Create time track component

--- ui/components/datasetItemWorkspace/src/components/VideoPlayer/VideoPlayer.svelte
+++ ui/components/datasetItemWorkspace/src/components/VideoPlayer/VideoPlayer.svelte
@@ -18,8 +18,10 @@
 
   import { SliderRoot } from "@pixano/core";
   import { itemObjects } from "../../lib/stores/datasetItemWorkspaceStores";
-  import { getCurrentImageTime, getImageIndexFromMouseMove } from "../../lib/api/videoApi";
+  import { getCurrentImageTime } from "../../lib/api/videoApi";
+
   import ObjectsTimescale from "./ObjectsTimescale.svelte";
+  import TimeTrack from "./TimeTrack.svelte";
 
   const imageFiles = import.meta.glob("../../assets/videos/mock/*.png") || {};
 
@@ -34,7 +36,6 @@
   let zoomLevel: number[] = [0];
 
   const videoTotalLengthInMs = Object.keys(imageFiles).length * videoSpeed;
-  let timeScaleInMs = [...Array(Math.floor(videoTotalLengthInMs / 100)).keys()];
 
   onMount(() => {
     updateView(currentImageIndex);
@@ -57,26 +58,6 @@
   };
 
   $: currentTime = getCurrentImageTime(currentImageIndex, videoSpeed);
-
-  const dragMe = (node: HTMLButtonElement) => {
-    let moving = false;
-
-    node.addEventListener("mousedown", () => {
-      moving = true;
-      clearInterval(intervalId);
-    });
-
-    window.addEventListener("mousemove", (event) => {
-      if (moving) {
-        currentImageIndex = getImageIndexFromMouseMove(event, node, Object.keys(imageFiles).length);
-        updateView(currentImageIndex);
-      }
-    });
-
-    window.addEventListener("mouseup", () => {
-      moving = false;
-    });
-  };
 
   const onPlayerClick = (event: MouseEvent | KeyboardEvent) => {
     let targetElement = event.target as HTMLElement;
@@ -116,50 +97,15 @@
         {/if}
       </button>
     </div>
-    <div class="p-2 w-full border-b border-slate-200 sticky top-0 bg-white z-10">
-      <div
-        class="flex w-full justify-between relative border-b border-slate-200 pt-8 cursor-pointer"
-        style={`width: ${zoomLevel[0]}%`}
-        role="slider"
-        tabindex="0"
-        on:click={onPlayerClick}
-        on:keydown={onPlayerClick}
-        aria-valuenow={currentImageIndex}
-      >
-        <button
-          use:dragMe
-          class="h-8 w-1 absolute z-10 bottom-0 flex flex-col translate-x-[-4px]"
-          style={`left: ${((currentImageIndex * videoSpeed) / videoTotalLengthInMs) * 100}%`}
-          bind:this={cursorElement}
-        >
-          <span class="block h-[60%] bg-primary w-2 rounded-t" />
-          <span
-            class="block w-0 h-0 border-l-[4px] border-l-transparent border-t-[8px] border-t-primary border-r-[4px] border-r-transparent"
-          >
-          </span>
-          <span class="w-[1px] bg-primary absolute ml-1" />
-        </button>
-        {#each timeScaleInMs as ms}
-          {#if ms % 10 === 0}
-            <span
-              class="absolute translate-x-[-50%] text-slate-300 w-[1px] h-1 bg-slate-500 bottom-0 pointer-events-none"
-              style={`left: ${((ms * 100) / videoTotalLengthInMs) * 100}%`}
-            />
-            {#if ms > 0}
-              <span
-                class="absolute translate-x-[-50%] text-slate-300 bottom-1 pointer-events-none font-light text-xs"
-                style={`left: ${((ms * 100) / videoTotalLengthInMs) * 100}%`}>{ms / 10}s</span
-              >
-            {/if}
-          {:else}
-            <span
-              class="absolute translate-x-[-50%] text-slate-300 w-[1px] h-1 bg-slate-300 bottom-0 pointer-events-none"
-              style={`left: ${((ms * 100) / videoTotalLengthInMs) * 100}%`}
-            />
-          {/if}
-        {/each}
-      </div>
-    </div>
+    <TimeTrack
+      {updateView}
+      {currentImageIndex}
+      imageFilesLength={Object.keys(imageFiles).length}
+      {videoSpeed}
+      {intervalId}
+      bind:cursorElement
+      {zoomLevel}
+    />
     <!-- bottom section -->
     <div class="grow flex flex-col max-h-[150px] gap-2 col-span-2">
       {#each Object.values($itemObjects) as object}
