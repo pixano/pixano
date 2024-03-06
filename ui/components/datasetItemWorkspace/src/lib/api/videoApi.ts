0aab5cb41d44684049628f7eea19e4373ef64cae
Timothee Reynaud <timothee.r@lajavaness.com>
2024-03-06 14:01:50 +0100

    Remove console.log statement in videoApi.ts

--- ui/components/datasetItemWorkspace/src/lib/api/videoApi.ts
+++ ui/components/datasetItemWorkspace/src/lib/api/videoApi.ts
@@ -35,7 +35,6 @@ export const linearInterpolation = (
   endIndex = endIndex < 0 ? currentInterval.breakPoints.length - 1 : endIndex;
   const start = currentInterval.breakPoints[endIndex - 1] || currentInterval.breakPoints[0];
   const end = currentInterval.breakPoints[endIndex];
-  console.log({ currentInterval, breakPointIntervals, imageIndex, endIndex, start, end });
   const { x: startX, y: startY } = start;
   const { x: endX, y: endY } = end;
   const xInterpolation = (endX - startX) / (end.frameIndex - start?.frameIndex);
