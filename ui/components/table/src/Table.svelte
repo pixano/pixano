2cb173b6ac532c33155f5601504432c3a9435b6c
ulysse-bonneau <ulysse.bonneau@proton.me>
2024-04-22 14:39:05 +0200

    fix select item in table

--- ui/components/table/src/Table.svelte
+++ ui/components/table/src/Table.svelte
@@ -196,7 +196,14 @@
           <tr
             {...rowAttrs}
             class="h-24 cursor-pointer hover:bg-slate-100"
-            on:click={() => handleSelectItem(row.id)}
+            on:click={() => {
+              for (const feature of items[row.id]) {
+                if (feature.name === "id") {
+                  handleSelectItem(feature.value);
+                  return;
+                }
+              }
+            }}
           >
             {#each row.cells as cell (cell.id)}
               <Subscribe attrs={cell.attrs()} let:attrs>
