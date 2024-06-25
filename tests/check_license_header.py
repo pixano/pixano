846d186661f5666266bea15f38b1237fbf330770
ulysse-bonneau <ulysse.bonneau@proton.me>
2024-06-25 10:33:36 +0200

    fix script format

--- tests/check_license_header.py
+++ tests/check_license_header.py
@@ -2,30 +2,41 @@ import os
 
 # Headers for each file type
 HEADERS = {
-    ".py": r"# =====================================\n# Copyright: CEA-LIST/DIASI/SIALV/LVA\n# Author : pixano@cea.fr\n# License: CECILL-C\n# =====================================",
-    ".svelte": r"<!-------------------------------------\nCopyright: CEA-LIST/DIASI/SIALV/LVA\nAuthor : pixano@cea.fr\nLicense: CECILL-C\n-------------------------------------->",
-    ".ts": r"/\*-------------------------------------\nCopyright: CEA-LIST/DIASI/SIALV/LVA\nAuthor : pixano@cea.fr\nLicense: CECILL-C\n-------------------------------------\*/",
+    ".py": r"# =====================================\n# Copyright: CEA-LIST/DIASI/SIALV/LVA\n\
+    # Author : pixano@cea.fr\n# License: CECILL-C\n# =====================================",
+    ".svelte": r"<!-------------------------------------\nCopyright: CEA-LIST/DIASI/SIALV/LVA\n\
+        Author : pixano@cea.fr\nLicense: CECILL-C\n-------------------------------------->",
+    ".ts": r"/\*-------------------------------------\nCopyright: CEA-LIST/DIASI/SIALV/LVA\n\
+        Author : pixano@cea.fr\nLicense: CECILL-C\n-------------------------------------\*/",
 }
 
 # Files to exclude
-EXCLUDE_FILES = {"vite.config.ts", "vite-env.d.ts", "vite.env.d.ts", "ambient.d.ts", "$types.d.ts"}
+EXCLUDE_FILES = {
+    "vite.config.ts",
+    "vite-env.d.ts",
+    "vite.env.d.ts",
+    "ambient.d.ts",
+    "$types.d.ts",
+}
+
 
 def check_header(file_path, header):
     """Check if the file contains the required header."""
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         content = file.read()
         return content.startswith(header)
 
+
 def main():
     missing_headers = []
-    for root, _, files in os.walk('.'):
+    for root, _, files in os.walk("."):
         for file in files:
             file_path = os.path.join(root, file)
             _, ext = os.path.splitext(file)
             if ext in HEADERS and file not in EXCLUDE_FILES:
                 if not check_header(file_path, HEADERS[ext]):
                     missing_headers.append(file_path)
-    
+
     if missing_headers:
         print("The following files miss the required license header :")
         for file in missing_headers:
@@ -33,6 +44,7 @@ def main():
         exit(1)
     else:
         print("All files include the required license header.")
+
 
 if __name__ == "__main__":
     main()
