4acf8976b3fa7e30c1579d734f8acfb5fac8e176
brenault <bertrand.renault@cea.fr>
2024-07-03 19:12:55 +0200

    lint

--- ui/components/datasetItemWorkspace/src/lib/api/videoApi.ts
+++ ui/components/datasetItemWorkspace/src/lib/api/videoApi.ts
@@ -291,53 +291,57 @@ export const mapTrackItemsToObject = (
   );
 
   const boxes: VideoObject["boxes"] = [];
-  for (const displayedBox of object.displayedMBox) {
-    boxes.push(
-      object.boxes
-        ? allItems.map((item) => {
-            const box = {
-              ...displayedBox,
-              frame_index: item.frame_index,
-              tracklet_id: item.tracklet_id,
-              is_key: true,
-            } as VideoItemBBox;
-            const currentBox =
-              object.boxes?.find(
-                (box) =>
-                  box.view_id == displayedBox.view_id && box.frame_index === item.frame_index,
-              ) || box;
-            return {
-              ...item,
-              ...currentBox,
-            };
-          })
-        : undefined,
-    );
+  if (object.displayedMBox) {
+    for (const displayedBox of object.displayedMBox) {
+      boxes.push(
+        object.boxes
+          ? allItems.map((item) => {
+              const box = {
+                ...displayedBox,
+                frame_index: item.frame_index,
+                tracklet_id: item.tracklet_id,
+                is_key: true,
+              } as VideoItemBBox;
+              const currentBox =
+                object.boxes?.find(
+                  (box) =>
+                    box.view_id == displayedBox.view_id && box.frame_index === item.frame_index,
+                ) || box;
+              return {
+                ...item,
+                ...currentBox,
+              } as VideoItemBBox;
+            })
+          : undefined,
+      );
+    }
   }
   const keypoints: VideoObject["keypoints"] = [];
-  for (const displayedKeypoints of object.displayedMKeypoints) {
-    keypoints.push(
-      object.keypoints
-        ? allItems.map((item) => {
-            const keypoint = {
-              ...displayedKeypoints,
-              frame_index: rightClickFrameIndex,
-              is_key: true,
-            } as VideoKeypoints;
-            const currentKeypoint =
-              object.keypoints?.find(
-                (kpt) =>
-                  kpt.view_id == displayedKeypoints.view_id && kpt.frame_index === item.frame_index,
-              ) || keypoint;
-            return {
-              ...item,
-              ...currentKeypoint,
-            };
-          })
-        : undefined,
-    );
+  if (object.displayedMKeypoints) {
+    for (const displayedKeypoints of object.displayedMKeypoints) {
+      keypoints.push(
+        object.keypoints
+          ? allItems.map((item) => {
+              const keypoint = {
+                ...displayedKeypoints,
+                frame_index: rightClickFrameIndex,
+                is_key: true,
+              } as VideoKeypoints;
+              const currentKeypoint =
+                object.keypoints?.find(
+                  (kpt) =>
+                    kpt.view_id == displayedKeypoints.view_id &&
+                    kpt.frame_index === item.frame_index,
+                ) || keypoint;
+              return {
+                ...item,
+                ...currentKeypoint,
+              } as VideoKeypoints;
+            })
+          : undefined,
+      );
+    }
   }
-
   return { boxes, keypoints };
 };
 
@@ -352,52 +356,56 @@ export const mapSplittedTrackToObject = (
   );
 
   const boxes: VideoObject["boxes"] = [];
-  for (const displayedBox of object.displayedMBox) {
-    boxes.push(
-      object.boxes
-        ? allItems.map((item) => {
-            const box = {
-              ...displayedBox,
-              frame_index: rightClickFrameIndex,
-              is_key: true,
-            } as VideoItemBBox;
-            const currentBox =
-              object.boxes?.find(
-                (box) =>
-                  box.view_id == displayedBox.view_id && box.tracklet_id === item.tracklet_id,
-              ) || box;
-            return {
-              ...item,
-              ...currentBox,
-            };
-          })
-        : undefined,
-    );
+  if (object.displayedMBox) {
+    for (const displayedBox of object.displayedMBox) {
+      boxes.push(
+        object.boxes
+          ? allItems.map((item) => {
+              const box = {
+                ...displayedBox,
+                frame_index: rightClickFrameIndex,
+                is_key: true,
+              } as VideoItemBBox;
+              const currentBox =
+                object.boxes?.find(
+                  (box) =>
+                    box.view_id == displayedBox.view_id && box.tracklet_id === item.tracklet_id,
+                ) || box;
+              return {
+                ...item,
+                ...currentBox,
+              } as VideoItemBBox;
+            })
+          : undefined,
+      );
+    }
   }
   const keypoints: VideoObject["keypoints"] = [];
-  for (const displayedKeypoints of object.displayedMKeypoints) {
-    keypoints.push(
-      object.keypoints
-        ? allItems.map((item) => {
-            const keypoint = {
-              ...displayedKeypoints,
-              frame_index: rightClickFrameIndex,
-              is_key: true,
-            } as VideoKeypoints;
-            const currentKeypoint =
-              object.keypoints?.find(
-                (kpt) =>
-                  kpt.view_id == displayedKeypoints.view_id && kpt.tracklet_id === item.tracklet_id,
-              ) || keypoint;
-            return {
-              ...item,
-              ...currentKeypoint,
-            };
-          })
-        : undefined,
-    );
+  if (object.displayedMKeypoints) {
+    for (const displayedKeypoints of object.displayedMKeypoints) {
+      keypoints.push(
+        object.keypoints
+          ? allItems.map((item) => {
+              const keypoint = {
+                ...displayedKeypoints,
+                frame_index: rightClickFrameIndex,
+                is_key: true,
+              } as VideoKeypoints;
+              const currentKeypoint =
+                object.keypoints?.find(
+                  (kpt) =>
+                    kpt.view_id == displayedKeypoints.view_id &&
+                    kpt.tracklet_id === item.tracklet_id,
+                ) || keypoint;
+              return {
+                ...item,
+                ...currentKeypoint,
+              } as VideoKeypoints;
+            })
+          : undefined,
+      );
+    }
   }
-
   return { boxes, keypoints };
 };
 
