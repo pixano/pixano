10fb089d5a1da425cd570b116c87b8e770bb717d
brenault <bertrand.renault@cea.fr>
2024-07-09 14:05:41 +0200

    lint

--- ui/tools/storybook/postcss.config.cjs
+++ ui/tools/storybook/postcss.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   plugins: {
-    'tailwindcss/nesting': {},
+    "tailwindcss/nesting": {},
     tailwindcss: {},
     autoprefixer: {},
   },
