0be5b1bb01575bb34f21a3b2fb8b877d690ba735
Julien Denize <40604584+juliendenize@users.noreply.github.com>
2024-07-10 14:44:09 +0200

    Refactor datasets tests (#299)

--- pixano/datasets/utils/image.py
+++ pixano/datasets/utils/image.py
@@ -24,9 +24,6 @@ def image_to_binary(image: Image.Image, im_format: str = "PNG") -> bytes:
     Returns:
         bytes: Image as binary
     """
-    if image is None:
-        return None
-
     with BytesIO() as output_bytes:
         image.save(output_bytes, im_format)
         im_bytes = output_bytes.getvalue()
@@ -50,7 +47,7 @@ def image_to_thumbnail(image: bytes | Image.Image) -> bytes:
     return image_to_binary(image)
 
 
-def binary_to_url(im_bytes: bytes | None) -> str:
+def binary_to_url(im_bytes: bytes) -> str:
     """Encode image from binary to base 64 URL.
 
     Args:
@@ -59,10 +56,8 @@ def binary_to_url(im_bytes: bytes | None) -> str:
     Returns:
         str: Image base 64 URL
     """
-    if im_bytes is not None:
-        encoded = base64.b64encode(im_bytes).decode("utf-8")
-        return f"data:image;base64,{encoded}"
-    return ""
+    encoded = base64.b64encode(im_bytes).decode("utf-8")
+    return f"data:image;base64,{encoded}"
 
 
 def depth_file_to_binary(depth_path: str) -> bytes:
@@ -116,7 +111,7 @@ def depth_array_to_gray(
     return depth_n
 
 
-def encode_rle(mask: list[list] | dict | None, height: int, width: int) -> dict:
+def encode_rle(mask: list[list] | dict, height: int, width: int) -> dict:
     """Encode mask from polygons / uncompressed RLE / RLE to RLE.
 
     Args:
@@ -133,10 +128,10 @@ def encode_rle(mask: list[list] | dict | None, height: int, width: int) -> dict:
         if isinstance(mask["counts"], list):
             return urle_to_rle(mask)
         return mask
-    return None
+    raise ValueError("Mask must be a list of polygons or an uncompressed RLE")
 
 
-def mask_to_rle(mask: Image.Image | None) -> dict:
+def mask_to_rle(mask: Image.Image | np.ndarray) -> dict:
     """Encode mask from Pillow or NumPy array to RLE.
 
     Args:
@@ -145,13 +140,11 @@ def mask_to_rle(mask: Image.Image | None) -> dict:
     Returns:
         dict: Mask as RLE
     """
-    if mask is not None:
-        mask_array = np.asfortranarray(mask)
-        return mask_api.encode(mask_array)
-    return None
+    mask_array = np.asfortranarray(mask)
+    return mask_api.encode(mask_array)
 
 
-def rle_to_mask(rle: dict[str, list[int] | bytes] | None) -> np.ndarray:
+def rle_to_mask(rle: dict[str, list[int] | bytes]) -> np.ndarray:
     """Decode mask from RLE to NumPy array.
 
     Args:
@@ -160,12 +153,10 @@ def rle_to_mask(rle: dict[str, list[int] | bytes] | None) -> np.ndarray:
     Returns:
         np.ndarray: Mask as NumPy array
     """
-    if rle is not None:
-        return mask_api.decode(rle)
-    return None
+    return mask_api.decode(rle)
 
 
-def polygons_to_rle(polygons: list[list] | None, height: int, width: int) -> dict:
+def polygons_to_rle(polygons: list[list], height: int, width: int) -> dict:
     """Encode mask from polygons to RLE.
 
     Args:
@@ -176,13 +167,11 @@ def polygons_to_rle(polygons: list[list] | None, height: int, width: int) -> dic
     Returns:
         dict: Mask as RLE
     """
-    if polygons is not None:
-        rles = mask_api.frPyObjects(polygons, height, width)
-        return mask_api.merge(rles)
-    return None
+    rles = mask_api.frPyObjects(polygons, height, width)
+    return mask_api.merge(rles)
 
 
-def rle_to_polygons(rle: dict[str, list[int] | bytes] | None) -> list[list]:
+def rle_to_polygons(rle: dict[str, list[int] | bytes]) -> list[list]:
     """Encode mask from RLE to polygons.
 
     Args:
@@ -191,17 +180,17 @@ def rle_to_polygons(rle: dict[str, list[int] | bytes] | None) -> list[list]:
     Returns:
         list[list]: Mask as polygons
     """
-    if rle is not None and "size" in rle:
-        h, w = rle["size"]
-        polygons, _ = mask_to_polygons(rle_to_mask(rle))
+    if "size" not in rle:
+        raise ValueError("RLE must have a size")
+    h, w = rle["size"]
+    polygons, _ = mask_to_polygons(rle_to_mask(rle))
 
-        # Normalize point coordinates
-        for p in polygons:
-            p[::2] = [x / w for x in p[::2]]
-            p[1::2] = [y / h for y in p[1::2]]
+    # Normalize point coordinates
+    for p in polygons:
+        p[::2] = [x / w for x in p[::2]]
+        p[1::2] = [y / h for y in p[1::2]]
 
-        return polygons
-    return None
+    return polygons
 
 
 def mask_to_polygons(mask: np.ndarray) -> tuple[list[list], bool]:
@@ -214,41 +203,38 @@ def mask_to_polygons(mask: np.ndarray) -> tuple[list[list], bool]:
         list[list]: Mask as polygons
         bool: True if mask has holes
     """
-    if mask is not None:
-        # Some versions of cv2 does not support incontiguous arr
-        mask = np.ascontiguousarray(mask)
+    # Some versions of cv2 does not support incontiguous arr
+    mask = np.ascontiguousarray(mask)
 
-        # cv2.RETR_CCOMP flag retrieves all the contours and arranges them
-        # to a 2-level hierarchy.
-        # External contours (boundary) of the object are placed in hierarchy-1.
-        # Internal contours (holes) are placed in hierarchy-2.
-        # cv2.CHAIN_APPROX_NONE flag gets vertices of polygons from contours.
-        res = cv2.findContours(
-            mask.astype("uint8"), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE
-        )
-        hierarchy = res[-1]
+    # cv2.RETR_CCOMP flag retrieves all the contours and arranges them
+    # to a 2-level hierarchy.
+    # External contours (boundary) of the object are placed in hierarchy-1.
+    # Internal contours (holes) are placed in hierarchy-2.
+    # cv2.CHAIN_APPROX_NONE flag gets vertices of polygons from contours.
+    res = cv2.findContours(
+        mask.astype("uint8"), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE
+    )
+    hierarchy = res[-1]
 
-        # If mask is empty
-        if hierarchy is None:
-            return [], False
+    # If mask is empty
+    if hierarchy is None:
+        return [], False
 
-        # Check if mask has holes
-        has_holes = (hierarchy.reshape(-1, 4)[:, 3] >= 0).sum() > 0
+    # Check if mask has holes
+    has_holes = (hierarchy.reshape(-1, 4)[:, 3] >= 0).sum() > 0
 
-        res = res[-2]
-        res = [x.flatten() for x in res]
+    res = res[-2]
+    res = [x.flatten() for x in res]
 
-        # The coordinates from OpenCV are integers in range [0, W-1 or H-1].
-        # We add 0.5 to turn them into real-value coordinate space. A better solution
-        # would be to first +0.5 and then dilate the returned polygon by 0.5.
-        res = [x + 0.5 for x in res if len(x) >= 6]
+    # The coordinates from OpenCV are integers in range [0, W-1 or H-1].
+    # We add 0.5 to turn them into real-value coordinate space. A better solution
+    # would be to first +0.5 and then dilate the returned polygon by 0.5.
+    res = [x + 0.5 for x in res if len(x) >= 6]
 
-        # Convert np.array to lists
-        res = [x.tolist() for x in res]
+    # Convert np.array to lists
+    res = [x.tolist() for x in res]
 
-        return res, has_holes
-
-    return [], False
+    return res, has_holes
 
 
 def urle_to_rle(urle: dict[str, list[int]]) -> dict[str, list[int] | bytes]:
@@ -260,10 +246,8 @@ def urle_to_rle(urle: dict[str, list[int]]) -> dict[str, list[int] | bytes]:
     Returns:
         dict[str, list[int] | bytes]: Mask as RLE
     """
-    if urle is not None:
-        height, width = urle["size"]
-        return mask_api.frPyObjects(urle, height, width)
-    return None
+    height, width = urle["size"]
+    return mask_api.frPyObjects(urle, height, width)
 
 
 def rle_to_urle(rle: dict[str, list[int] | bytes]) -> dict[str, list[int]]:
@@ -275,12 +259,12 @@ def rle_to_urle(rle: dict[str, list[int] | bytes]) -> dict[str, list[int]]:
     Returns:
         dict[str, list[int]]: Mask as uncompressed RLE
     """
-    if rle is not None and rle["counts"] is not None:
-        mask = rle_to_mask(rle)
-        urle = {"counts": [], "size": list(mask.shape)}
+    if "counts" not in rle or rle["counts"] is None:
+        raise ValueError("RLE must have counts")
+    mask = rle_to_mask(rle)
+    urle = {"counts": [], "size": list(mask.shape)}
 
-        for i, (value, elements) in enumerate(groupby(mask.ravel(order="F"))):
-            urle["counts"].append(0 if i == 0 and value == 1 else len(list(elements)))
+    for i, (value, elements) in enumerate(groupby(mask.ravel(order="F"))):
+        urle["counts"].append(0 if i == 0 and value == 1 else len(list(elements)))
 
-        return urle
-    return None
+    return urle
