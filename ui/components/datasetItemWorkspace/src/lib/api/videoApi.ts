90a586c164860e877284070b93ee6dacae4436fe
Corentin Vannier <corentin.vannier@cea.fr>
2024-11-29 12:05:41 +0100

    fix(front): Use the correct time format mm:ss.fff
    Use `.` instead of `:` between seconds and milliseconds

--- ui/components/datasetItemWorkspace/src/lib/api/videoApi.ts
+++ ui/components/datasetItemWorkspace/src/lib/api/videoApi.ts
@@ -18,7 +18,7 @@ export const getCurrentImageTime = (imageIndex: number, videoSpeed: number) => {
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
   const milliseconds = String(date.getMilliseconds()).padStart(3, "0");
-  return `${minutes}:${seconds}:${milliseconds}`;
+  return `${minutes}:${seconds}.${milliseconds}`;
 };
 
 export const getImageIndexFromMouseMove = (
