fc3d4b08701e61ca9e3ef4e4c643db5d9176c6bd
antoningaladrim <166378875+antoningaladrim@users.noreply.github.com>
2024-12-09 17:07:04 +0100

    feat: clean components config

--- ui/components/datasetItemWorkspace/tailwind.config.cjs
+++ ui/components/datasetItemWorkspace/tailwind.config.cjs
@@ -2,14 +2,7 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{svelte,js,ts}",
-    "../canvas2d/src/**/*.{svelte,js,ts}",
-    "../canvas3d/src/**/*.{svelte,js,ts}",
-    "../core/src/**/*.{svelte,js,ts}",
-    "../../apps/annotator/src/.{svelte,js,ts}",
-  ],
+  content: ["./index.html", "./src/**/*.{svelte,js,ts}"],
   darkMode: "media", // or 'class'
   // theme: {
   //   extend: {
