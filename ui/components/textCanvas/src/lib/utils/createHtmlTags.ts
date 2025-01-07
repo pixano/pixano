c947aba90d6b7123e5af32fc5e82c69ddfaa7c25
antoningaladrim <166378875+antoningaladrim@users.noreply.github.com>
2025-01-07 16:21:25 +0100

    refacto: do not create html tags concatening string to prevent from XSS injections

--- ui/components/textCanvas/src/lib/utils/createHtmlTags.ts
+++ ui/components/textCanvas/src/lib/utils/createHtmlTags.ts
@@ -4,30 +4,36 @@ Author : pixano@cea.fr
 License: CECILL-C
 -------------------------------------*/
 
+const TAG_NAME = "span";
+
 export const createHtmlTags = ({
+  content,
   metadata,
   bgColor,
   hidden,
-  tagName = "span",
 }: {
+  content: string;
   metadata: Record<string, string>;
   bgColor: string;
   hidden: boolean;
-  tagName?: string;
 }) => {
-  let openTag = `<${tagName}`;
+  const element = document.createElement(TAG_NAME);
+  
+  element.style.backgroundColor = bgColor;
+  element.style.color = "white";
+  element.style.padding = "1px 4px";
+  element.style.borderRadius = "6px";
+  element.style.margin = "0px 1px";
+
+  element.innerText = content;
+
+  if (hidden) {
+    element.style.backgroundColor = "transparent";
+  }
 
   for (const [key, value] of Object.entries(metadata)) {
-    openTag += ` data-${key}="${value}"`;
+    element.dataset[key] = value;
   }
 
-  if (!hidden) {
-    openTag += ` class="text-white rounded-md px-1 py-px mx-px" style="background:${hidden ? "transparent" : bgColor};"`;
-  }
-
-  openTag += `>`;
-
-  const closeTag = `</${tagName}>`;
-
-  return { openTag, closeTag };
+  return element.outerHTML;
 };
