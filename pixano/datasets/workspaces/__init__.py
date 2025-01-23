8e758fffaaf13aad27ff3660e5a7f0d3495e4ad4
brenault <bertrand.renault@cea.fr>
2025-01-23 13:46:26 +0100

    remove workspace classes

--- pixano/datasets/workspaces/__init__.py
+++ pixano/datasets/workspaces/__init__.py
@@ -5,13 +5,6 @@
 # =====================================
 
 from enum import Enum
-from typing import Literal, overload
-
-from .image import ImageWorkspace
-from .image_text_entity_linking import ImageTextEntityLinkingWorkspace
-from .image_vqa_workspace import ImageVQAWorkspace
-from .video import VideoWorkspace
-from .workspace import Workspace
 
 
 class WorkspaceType(Enum):
@@ -34,53 +27,6 @@ class WorkspaceType(Enum):
     UNDEFINED = "undefined"
 
 
-@overload
-def instatiate_workspace_by_type(
-    workspace_type: Literal[WorkspaceType.IMAGE] = WorkspaceType.IMAGE,
-) -> ImageWorkspace: ...
-@overload
-def instatiate_workspace_by_type(
-    workspace_type: Literal[WorkspaceType.VIDEO] = WorkspaceType.VIDEO,
-) -> VideoWorkspace: ...
-@overload
-def instatiate_workspace_by_type(
-    workspace_type: Literal[WorkspaceType.IMAGE_VQA] = WorkspaceType.IMAGE_VQA,
-) -> ImageVQAWorkspace: ...
-@overload
-def instatiate_workspace_by_type(
-    workspace_type: Literal[WorkspaceType.IMAGE_TEXT_ENTITY_LINKING] = WorkspaceType.IMAGE_TEXT_ENTITY_LINKING,
-) -> ImageTextEntityLinkingWorkspace: ...
-@overload
-def instatiate_workspace_by_type(
-    workspace_type: Literal[WorkspaceType.UNDEFINED] = WorkspaceType.UNDEFINED,
-) -> Workspace: ...
-def instatiate_workspace_by_type(
-    workspace_type: Literal[WorkspaceType.IMAGE]
-    | Literal[WorkspaceType.VIDEO]
-    | Literal[WorkspaceType.IMAGE_VQA]
-    | Literal[WorkspaceType.IMAGE_TEXT_ENTITY_LINKING]
-    | Literal[WorkspaceType.UNDEFINED] = WorkspaceType.UNDEFINED,
-) -> ImageWorkspace | VideoWorkspace | ImageVQAWorkspace | ImageTextEntityLinkingWorkspace | Workspace:
-    """Instatiate workspace by type."""
-    match workspace_type:
-        case WorkspaceType.IMAGE:
-            return ImageWorkspace()
-        case WorkspaceType.VIDEO:
-            return VideoWorkspace()
-        case WorkspaceType.IMAGE_VQA:
-            return ImageVQAWorkspace()
-        case WorkspaceType.IMAGE_TEXT_ENTITY_LINKING:
-            return ImageTextEntityLinkingWorkspace()
-        case _:
-            raise ValueError("Workspace type not found")
-
-
 __all__ = [
-    "ImageWorkspace",
-    "ImageTextEntityLinkingWorkspace",
-    "ImageVQAWorkspace",
-    "VideoWorkspace",
-    "Workspace",
     "WorkspaceType",
-    "instatiate_workspace_by_type",
 ]
