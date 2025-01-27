4a9b9e5fc971ddf549fcd6ad601e56a75ae9a82b
antoningaladrim <166378875+antoningaladrim@users.noreply.github.com>
2025-01-27 16:31:46 +0100

    fix: add missing licence headers

--- ui/tools/storybook/stories/testing/index.ts
+++ ui/tools/storybook/stories/testing/index.ts
@@ -1,3 +1,9 @@
+/*-------------------------------------
+Copyright: CEA-LIST/DIASI/SIALV/LVA
+Author : pixano@cea.fr
+License: CECILL-C
+-------------------------------------*/
+
 export * from "./mockHandleSaveItem";
 export * from "./MockInteractiveImageSegmenter";
 export * from "./shared";
