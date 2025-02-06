42462c16b4ad42b1703ed8f4328f16c2c6378fcb
antoningaladrim <166378875+antoningaladrim@users.noreply.github.com>
2025-02-06 15:00:38 +0100

    feat: use pixanoSource for newly created answers

--- ui/components/datasetItemWorkspace/src/lib/utils/createNewAnswer.ts
+++ ui/components/datasetItemWorkspace/src/lib/utils/createNewAnswer.ts
@@ -6,6 +6,8 @@ License: CECILL-C
 
 import { BaseSchema, Message, MessageTypeEnum, type MessageType } from "@pixano/core";
 import { nanoid } from "nanoid";
+import { sourcesStore } from "../../../../../apps/pixano/src/lib/stores/datasetStores";
+import { getPixanoSource } from "../api/objectsApi";
 
 export const createNewAnswer = ({
   question,
@@ -19,6 +21,7 @@ export const createNewAnswer = ({
   explanations?: string[];
 }) => {
   const now = new Date().toISOString();
+  const pixanoSource = getPixanoSource(sourcesStore);
 
   const messageData: MessageType = {
     number: question.data.number,
@@ -35,6 +38,10 @@ export const createNewAnswer = ({
     table_info: { name: "messages", group: "annotations", base_schema: BaseSchema.Message },
     created_at: now,
     updated_at: now,
-    data: { ...question.data, ...messageData },
+    data: {
+      ...question.data,
+      ...messageData,
+      source_ref: { name: pixanoSource.table_info.name, id: pixanoSource.id },
+    },
   });
 };
