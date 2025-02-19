dfd9a2a902c84bce210f339aeaabfae8a86df127
antoningaladrim <166378875+antoningaladrim@users.noreply.github.com>
2025-02-19 17:33:25 +0100

    fix: add missing licence header

--- ui/components/vqaCanvas/src/lib/types/StoreQuestionEvent.ts
+++ ui/components/vqaCanvas/src/lib/types/StoreQuestionEvent.ts
@@ -1,3 +1,9 @@
+/*-------------------------------------
+Copyright: CEA-LIST/DIASI/SIALV/LVA
+Author : pixano@cea.fr
+License: CECILL-C
+-------------------------------------*/
+
 import type { QuestionTypeEnum } from "@pixano/core";
 
 export type StoreQuestionEvent = {
