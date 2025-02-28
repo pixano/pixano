d376b398831a5da44208fe9638cc56387b36a210
antoningaladrim <166378875+antoningaladrim@users.noreply.github.com>
2025-02-28 19:32:29 +0100

    fix lint

--- ui/components/datasetItemWorkspace/src/lib/utils/removeUiFieldFromObject.ts
+++ ui/components/datasetItemWorkspace/src/lib/utils/removeUiFieldFromObject.ts
@@ -4,6 +4,7 @@ Author : pixano@cea.fr
 License: CECILL-C
 -------------------------------------*/
 
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 export const removeFieldFromObject = <T extends Record<string, any>, K extends keyof T>(
   object: T,
   field: K,
@@ -11,6 +12,7 @@ export const removeFieldFromObject = <T extends Record<string, any>, K extends k
   if (!(field in object)) {
     return object;
   }
+  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
   const { [field]: _, ...rest } = object;
 
   return rest;
