1dbcb6bfdb99223c7c0e49b2f7b98d0b7ab39fda
BertrandRenault <112549869+BertrandRenault@users.noreply.github.com>
2025-04-01 00:16:44 +0200

    filter unique mentions (#521)

--- ui/components/canvas2d/src/components/TextSpansContent.svelte
+++ ui/components/canvas2d/src/components/TextSpansContent.svelte
@@ -5,17 +5,28 @@ License: CECILL-C
 -------------------------------------->
 
 <script lang="ts">
-  import { BaseSchema, type Annotation } from "@pixano/core";
+  import { BaseSchema, TextSpan, type Annotation } from "@pixano/core";
 
   export let annotations: Annotation[] | undefined;
+
+  const uniqueMentions = (): string[] => {
+    const mentions = new Set<string>();
+    const textSpans = annotations?.filter((ann) => ann.is_type(BaseSchema.TextSpan)) ?? [];
+    (textSpans as TextSpan[]).forEach((tspan) => {
+      mentions.add(tspan.data.mention);
+    });
+    return Array.from(mentions.values());
+  };
+
+  const mentions = uniqueMentions();
 </script>
 
 {#if annotations?.some((ann) => ann.is_type(BaseSchema.TextSpan))}
   <p class="font-medium mt-4">Text spans</p>
   <div class="grid gap-x-4 gap-y-2 grid-cols-[150px_auto] mt-2 pr-4">
-    {#each annotations?.filter((ann) => ann.is_type(BaseSchema.TextSpan)) ?? [] as textSpan}
+    {#each mentions as mention}
       <p class="font-medium">mention</p>
-      <p>{textSpan.data.mention}</p>
+      <p>{mention}</p>
     {/each}
   </div>
 {/if}
