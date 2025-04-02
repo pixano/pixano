380158f73a7fdd9190f7424dce5b4943c8a08588
BertrandRenault <112549869+BertrandRenault@users.noreply.github.com>
2025-04-02 15:25:41 +0200

    allow schema overwrite (#524)
    * allow schema overwrite
    
    * test coverage
    
    * depends on mode and exist_ok to raise or not
    
    * exist_ok OR mode ovewrite

--- tests/datasets/test_dataset_schema.py
+++ tests/datasets/test_dataset_schema.py
@@ -400,6 +400,8 @@ class TestDatasetSchema:
         assert schema.relations["new_table"] == {"item": relation}
         assert set(schema.groups[SchemaGroup.VIEW]) == {"image", "new_table"}
         assert schema.relations["item"]["new_table"] == item_relation
+        schema = dataset_schema_item_categories_image_bbox.add_schema("image", Image, relation, overwrite_schema=True)
+        assert schema.relations["item"]["image"] == item_relation
 
     def test_add_error(self, dataset_schema_item_categories_image_bbox):
         with pytest.raises(ValueError, match="Table image already exists in the schemas."):
