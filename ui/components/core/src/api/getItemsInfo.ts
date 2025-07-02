68b8316fec4dd74d0aef4e3d477ac21bf23682fc
BertrandRenault <112549869+BertrandRenault@users.noreply.github.com>
2025-07-02 11:55:41 +0200

    Display more info in datasets library (#570)

---
+++ ui/components/core/src/api/getItemsInfo.ts
@@ -0,0 +1,31 @@
+/*-------------------------------------
+Copyright: CEA-LIST/DIASI/SIALV/LVA
+Author : pixano@cea.fr
+License: CECILL-C
+-------------------------------------*/
+
+import type { DatasetMoreInfo } from "../lib/types/dataset/DatasetMoreInfo";
+
+export async function getItemsInfo(datasetId: string): Promise<Array<DatasetMoreInfo>> {
+  let infos: Array<DatasetMoreInfo>;
+
+  try {
+    const response = await fetch(`/items_info/${datasetId}`);
+    if (response.ok) {
+      infos = (await response.json()) as Array<DatasetMoreInfo>;
+    } else {
+      infos = [];
+      console.log(
+        "api.getItemsInfo -",
+        response.status,
+        response.statusText,
+        await response.text(),
+      );
+    }
+  } catch (e) {
+    infos = [];
+    console.log("api.getItemsInfo -", e);
+  }
+
+  return infos;
+}
