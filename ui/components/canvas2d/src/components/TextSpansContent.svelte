826e88f8d9b1859f70ad7123d56984b0d0bbcd64
brenault <bertrand.renault@cea.fr>
2025-07-08 11:18:56 +0200

    chevron for features + textspan.data.role value instead of mention

--- ui/components/canvas2d/src/components/TextSpansContent.svelte
+++ ui/components/canvas2d/src/components/TextSpansContent.svelte
@@ -6,14 +6,23 @@ License: CECILL-C
 
 <script lang="ts">
   import { BaseSchema, TextSpan, type Annotation } from "@pixano/core";
+  import { DISPLAY_MENTION_FEATURES } from "@pixano/dataset-item-workspace/src/lib/settings/defaultFeatures";
 
   export let annotations: Annotation[] | undefined;
 
-  const uniqueMentions = (): string[] => {
-    const mentions = new Set<string>();
+  const uniqueMentions = (): [string, string][] => {
+    const mentions: [string, string][] = [];
     const textSpans = annotations?.filter((ann) => ann.is_type(BaseSchema.TextSpan)) ?? [];
     (textSpans as TextSpan[]).forEach((tspan) => {
-      mentions.add(tspan.data.mention);
+      let mention_label: string = "mention";
+      for (const feat of DISPLAY_MENTION_FEATURES) {
+        if (feat in tspan.data && typeof tspan.data[feat] === "string") {
+          mention_label = tspan.data[feat];
+          break;
+        }
+      }
+      if (!mentions.find(([ml, m]) => ml === mention_label && m === tspan.data.mention))
+        mentions.push([mention_label, tspan.data.mention]);
     });
     return Array.from(mentions.values());
   };
@@ -24,8 +33,8 @@ License: CECILL-C
 {#if annotations?.some((ann) => ann.is_type(BaseSchema.TextSpan))}
   <p class="font-medium mt-4">Text spans</p>
   <div class="grid gap-x-4 gap-y-2 grid-cols-[150px_auto] mt-2 pr-4">
-    {#each mentions as mention}
-      <p class="font-medium ml-4">mention</p>
+    {#each mentions as [mention_label, mention]}
+      <p class="font-medium ml-4">{mention_label}</p>
       <p>{mention}</p>
     {/each}
   </div>
