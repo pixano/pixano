1f6335c48c0fa3466dfc4d18146a34e5927b7254
Benjamin Labbé <benjamin.labbe@cea.fr>
2025-07-24 19:15:14 +0200

    Add a unit-test for get_thumbnail

---
+++ tests/app/routers/test_thumbail.py
@@ -0,0 +1,29 @@
+# =====================================
+# Copyright: CEA-LIST/DIASI/SIALV/LVA
+# Author : pixano@cea.fr
+# License: CECILL-C
+# =====================================
+
+import base64
+from pathlib import Path
+
+from fastapi.applications import FastAPI
+from fastapi.testclient import TestClient
+
+from pixano.app.settings import Settings
+from tests.assets.sample_data.metadata import IMAGE_JPG_ASSET_URL
+
+
+def test_get_thumbnail(app_and_settings_with_client: tuple[FastAPI, Settings, TestClient]):
+    app, settings, client = app_and_settings_with_client
+
+    # copy image in media_dir
+    basename = Path(IMAGE_JPG_ASSET_URL).name
+    encoded_basename = base64.b64encode(basename.encode("utf-8")).decode("utf-8")
+    max_size = 128
+
+    # url = app.url_path_for('get_thumbnail', b64_image_path=encoded_basename )
+    response = client.get(f"/thumbnail/{encoded_basename}?max_size={max_size}")
+    assert response.status_code == 200
+    assert response.headers["content-Type"] == "image/jpeg"
+    # assert response.content == ??
